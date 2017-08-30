78c08632fa86b91891d9612ce25a0effb0fb14f7
Edresson Casanova <edresson1@gmail.com>
2017-08-30 19:19:00 -0300

    Update Firewall.sh

--- Firewall.sh
+++ Firewall.sh
@@ -21,19 +21,15 @@ iptables -I INPUT  -s 162.144.7.0/24 -j ACCEPT
 iptables -I INPUT  -s 149.202.241.189 -j ACCEPT
 iptables -I INPUT  -s 149.202.241.0/24 -j ACCEPT
 
-iptables -I INPUT -p udp --dport 7777  -m  string --algo kmp   --hex-string   '|081e77da|'  -m recent --name nlimitcon7777 ! --rcheck  -m recent --name limitcon7777 --set -j DROP
-iptables -I INPUT  -p udp --dport 7777   -m  string --algo kmp   --hex-string   '|081e77da|' -m recent --name limitcon7777 --rcheck -m recent --name limitcon7777 --remove -m recent --name nlimitcon7777 --set -j ACCEPT
-iptables -I INPUT  -p udp --dport 7777   -m  string --algo kmp   --hex-string   '|081e77da|' -m recent --name nlimit7777 --rcheck -j ACCEPT	
+iptables -I INPUT -p udp --dport 7777  -m  string --algo kmp   --hex-string   '|081e77da|' -m recent --name test ! --rcheck  -m recent --name test --set -j DROP
+iptables -I INPUT -p udp --dport 7777  -m  string --algo kmp   --hex-string   '|081e77da|'  -m recent --name test --rcheck  --seconds 1 --hitcount 1   -j DROP
 
-iptables -I INPUT  -p udp --dport 7777  -m  string --algo kmp   --hex-string   '|53414d50|' -m  string --algo kmp   --hex-string   '|611e63|'  -m recent --name nlimitC7777 ! --rcheck  -m recent --name limitC7777 --set -j DROP
-iptables -I INPUT  -p udp --dport 7777   -m  string --algo kmp   --hex-string   '|53414d50|' -m  string --algo kmp   --hex-string   '|611e63|' -m recent --name limitC7777 --rcheck -m recent --name limitC7777 --remove -m recent --name nlimitC7777 --set -j ACCEPT
-iptables -I INPUT  -p udp --dport 7777   -m  string --algo kmp   --hex-string   '|53414d50|' -m  string --algo kmp   --hex-string   '|611e63|'  -m recent --name nlimitR7777 --rcheck -j ACCEPT	
-
-iptables -I INPUT  -p udp --dport 7777  -m  string --algo kmp   --hex-string   '|53414d50|' -m  string --algo kmp   --hex-string   '|611e72|'   -m recent --name nlimitR7777 ! --rcheck  -m recent --name limitR7777 --set -j DROP
-iptables -I INPUT   -p udp --dport 7777   -m  string --algo kmp   --hex-string   '|53414d50|' -m  string --algo kmp   --hex-string   '|611e72|'  -m recent --name limitR7777 --rcheck -m recent --name limitR7777 --remove -m recent --name nlimitR7777 --set -j ACCEPT
-iptables -I INPUT   -p udp --dport 7777   -m  string --algo kmp   --hex-string   '|53414d50|' -m  string --algo kmp   --hex-string   '|611e72|'  -m recent --name nlimitR7777 --rcheck -j ACCEPT	
+iptables -I INPUT  -p udp --dport 7777  -m  string --algo kmp   --hex-string   '|53414d50|' -m  string --algo kmp   --hex-string   '|611e63|'  -m recent --name limitC7777 ! --rcheck  -m recent --name limitC7777 --set -j DROP
+iptables -I INPUT  -p udp --dport 7777   -m  string --algo kmp   --hex-string   '|53414d50|' -m  string --algo kmp   --hex-string   '|611e63|' -m recent --name limitC7777 --rcheck  --seconds 1 --hitcount 1   -j DROP
 
 
-iptables -I INPUT  -p udp --dport 7777  -m  string --algo kmp   --hex-string   '|53414d50|' -m  string --algo kmp   --hex-string   '|611e69|'   -m recent --name nlimitI7777 ! --rcheck -m recent --name limitI7777 --set  -j DROP
-iptables -I INPUT   -p udp --dport 7777   -m  string --algo kmp   --hex-string  '|53414d50|' -m  string --algo kmp   --hex-string   '|611e69|'  -m recent --name limitI7777 --rcheck -m recent --name limitI7777 --remove -m recent --name nlimitI7777 --set -j ACCEPT
-iptables -I INPUT -p udp --dport 7777 -m string --algo kmp --hex-string '|53414d50|' -m  string --algo kmp   --hex-string   '|611e69|'  -m recent --name nlimitI7777 --rcheck -j ACCEPT
+iptables -I INPUT  -p udp --dport 7777  -m  string --algo kmp   --hex-string   '|53414d50|' -m  string --algo kmp   --hex-string   '|611e69|'  -m recent --name limitI7777 ! --rcheck  -m recent --name limitI7777 --set 
+iptables -I INPUT  -p udp --dport 7777   -m  string --algo kmp   --hex-string   '|53414d50|' -m  string --algo kmp   --hex-string   '|611e69|' -m recent --name limitI7777 --rcheck  --seconds 1 --hitcount 1   -j DROP
+
+iptables -I INPUT  -p udp --dport 7777  -m  string --algo kmp   --hex-string   '|53414d50|' -m  string --algo kmp   --hex-string   '|611e72|'  -m recent --name limitR7777 ! --rcheck  -m recent --name limitR7777 --set -j DROP
+iptables -I INPUT  -p udp --dport 7777   -m  string --algo kmp   --hex-string   '|53414d50|' -m  string --algo kmp   --hex-string   '|611e72|' -m recent --name limitR7777 --rcheck  --seconds 1 --hitcount 1   -j DROP

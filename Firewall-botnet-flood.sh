eed8c29311748f3476b868d61339d7b8720af54c
Edresson Casanova <edresson1@gmail.com>
2017-11-09 21:23:16 -0200

    Update Firewall-botnet-flood.sh

--- Firewall-botnet-flood.sh
+++ Firewall-botnet-flood.sh
@@ -1,17 +1,42 @@
-#Drop First packet for all ips connection on port 7777 and limit flood in 2 connection for seconds (if you pass this ip is blocked)
+# SAMP-Firewall
+# Layer 7 protection for samp servers
+# Credits: Edresson Casanova | BlastHosting  : https://www.blasthosting.com.br/ 
+# Thanks for: JernejL Beta Tester 
+#Working for SA:Mp servers in port 7777
 
-iptables -I INPUT -p udp --dport 7777  -m  string --algo kmp   --hex-string   '|081e77da|'  -m recent --name test --set 
-iptables -I INPUT -p udp --dport 7777  -m  string --algo kmp   --hex-string   '|081e77da|'  -m recent --name test --rcheck  --seconds 1 --hitcount 3  -m recent --name test --remove -m recent --name blacklist --set   -j DROP
+#modprobe ipt_recent ip_list_tot=10000
+iptables -F
 
-iptables -I INPUT  -p udp --dport 7777  -m  string --algo kmp   --hex-string   '|53414d50|' -m  string --algo kmp   --hex-string   '|611e63|'  -m recent --name testi --set 
-iptables -I INPUT  -p udp --dport 7777   -m  string --algo kmp   --hex-string   '|53414d50|' -m  string --algo kmp   --hex-string   '|611e63|' -m recent --name testi --rcheck  --seconds 1 --hitcount 3  -m recent --name testi --remove -m recent --name blacklist --set   -j DROP
+### Samp check server stats 	
+iptables -I INPUT  -s 66.55.155.101 -j ACCEPT
+iptables -I INPUT  -s 66.55.155.0/24 -j ACCEPT
+iptables -I INPUT  -s  82.192.84.116 -j ACCEPT
+iptables -I INPUT  -s  82.192.84.0/24 -j ACCEPT		
 
-iptables -I INPUT  -p udp --dport 7777  -m  string --algo kmp   --hex-string   '|53414d50|' -m  string --algo kmp   --hex-string   '|611e69|'  -m recent --name testr --set 
-iptables -I INPUT  -p udp --dport 7777   -m  string --algo kmp   --hex-string   '|53414d50|' -m  string --algo kmp   --hex-string   '|611e69|' -m recent --name testr --rcheck  --seconds 1 --hitcount 3 -m recent --name testr --remove -m recent --name blacklist --set   -j DROP
+iptables -t filter -A OUTPUT -p icmp -m icmp --icmp-type echo-reply -j DROP
+iptables -t filter -A OUTPUT -p icmp -m icmp --icmp-type port-unreachable -j DROP
 
 
-iptables -I INPUT  -p udp --dport 7777  -m  string --algo kmp   --hex-string   '|53414d50|' -m  string --algo kmp   --hex-string   '|611e72|'  -m recent --name testc --set 
-iptables -I INPUT  -p udp --dport 7777   -m  string --algo kmp   --hex-string   '|53414d50|' -m  string --algo kmp   --hex-string   '|611e72|' -m recent --name testc --rcheck  --seconds 1 --hitcount 3 -m recent --name testc --remove -m recent --name blacklist --set   -j DROP
+
+### SAcnr monitor and 
+iptables -I INPUT  -s 104.28.17.92 -j ACCEPT
+iptables -I INPUT  -s 104.28.17.0/24 -j ACCEPT
+#sa-mp.in
+iptables -I INPUT  -s 162.144.7.215 -j ACCEPT
+iptables -I INPUT  -s 162.144.7.0/24 -j ACCEPT
+#game-stats
+iptables -I INPUT  -s 149.202.241.189 -j ACCEPT
+iptables -I INPUT  -s 149.202.241.0/24 -j ACCEPT
+
+iptables -I INPUT -p udp --dport 7777  -m  string --algo kmp   --hex-string   '|081e77da|' -m recent --name test ! --rcheck  -m recent --name test --set   -j  DROP
+iptables -I INPUT -p udp --dport 7777  -m  string --algo kmp   --hex-string   '|081e77da|'  -m recent --name test --rcheck --seconds 2  --hitcount 1     -j DROP 
+
+iptables -I INPUT  -p udp --dport 7777  -m  string --algo kmp   --hex-string   '|53414d50|' -m  string --algo kmp   --hex-string   '|611e63|'  -m recent --name limitC7777 ! --rcheck  -m recent --name limitC7777 --set -j DROP
+iptables -I INPUT  -p udp --dport 7777   -m  string --algo kmp   --hex-string   '|53414d50|' -m  string --algo kmp   --hex-string   '|611e63|' -m recent --name limitC7777 --rcheck  --seconds 2 --hitcount 1   -j DROP
 
 
-iptables -I INPUT -p udp --dport 7777  -m recent --name blacklist --rcheck -j DROP
+iptables -I INPUT  -p udp --dport 7777  -m  string --algo kmp   --hex-string   '|53414d50|' -m  string --algo kmp   --hex-string   '|611e69|'  -m recent --name limitI7777 ! --rcheck  -m recent --name limitI7777 --set 
+iptables -I INPUT  -p udp --dport 7777   -m  string --algo kmp   --hex-string   '|53414d50|' -m  string --algo kmp   --hex-string   '|611e69|' -m recent --name limitI7777 --rcheck  --seconds 2 --hitcount 1   -j DROP
+
+iptables -I INPUT  -p udp --dport 7777  -m  string --algo kmp   --hex-string   '|53414d50|' -m  string --algo kmp   --hex-string   '|611e72|'  -m recent --name limitR7777 ! --rcheck  -m recent --name limitR7777 --set -j DROP
+iptables -I INPUT -p udp --dport 7777 -m string --algo kmp --hex-string '|53414d50|' -m string --algo kmp --hex-string '|611e72|' -m recent --name limitR7777 --rcheck --seconds 2 --hitcount 1 -j DROP
